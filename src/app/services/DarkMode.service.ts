68f8d96c8a24620953412fe29c8f3188fb5cab37
Alex <alexis.autres@gmail.com>
2023-03-22 02:54:41 +0100

    Update darkMode with user timezone

--- src/app/services/DarkMode.service.ts
+++ src/app/services/DarkMode.service.ts
@@ -6,7 +6,24 @@ import { Injectable } from '@angular/core';
 export class DarkModeService {
   isDarkMode = false;
 
+  constructor() {
+    this.updateTheme();
+  }
+
+  private updateTheme(): void {
+    const hours = new Date().getHours();
+    this.isDarkMode = hours >= 18 || hours <= 6; // Dark mode from 6 PM to 6 AM
+  }
+
   toggleDarkMode() {
     this.isDarkMode = !this.isDarkMode;
+    const icon = document.querySelector('.switch .slider i');
+    if (icon) {
+      if (this.isDarkMode) {
+        //icon.classList.add('fa-moon');
+      } else {
+        //icon.classList.add('fa-sun');
+      }
+    }
   }
 }
